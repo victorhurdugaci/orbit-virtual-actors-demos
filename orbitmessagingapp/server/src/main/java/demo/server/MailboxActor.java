de8f597c3a526c750a377d2b7a837e51a8aba174
Victor Hurdugaci <vhurdugaci@ea.com>
2017-09-04 12:11:06 -0700

    Remove state

--- orbitmessagingapp/server/src/main/java/demo/server/MailboxActor.java
+++ orbitmessagingapp/server/src/main/java/demo/server/MailboxActor.java
@@ -2,23 +2,22 @@ package demo.server;
 
 import cloud.orbit.actors.runtime.AbstractActor;
 import cloud.orbit.concurrent.Task;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import demo.shared.Mailbox;
 import demo.shared.Message;
 
-import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class MailboxActor extends AbstractActor<MailboxState> implements Mailbox {
-    private final ObjectMapper jsonMapper = new ObjectMapper();
+public class MailboxActor extends AbstractActor implements Mailbox {
+    private List<Message> messages = new ArrayList<>();
 
     @Override
     public Task<?> activateAsync() {
         printMsg("activateAsync called");
 
-        return readState();
+        return Task.done();
     }
 
     @Override
@@ -26,54 +25,6 @@ public class MailboxActor extends AbstractActor<MailboxState> implements Mailbox
         printMsg("deactivateAsync called");
 
         return super.deactivateAsync();
-    }
-
-    @Override
-    protected Task<Boolean> readState() {
-        return Task.supplyAsync(() -> {
-            File inputFile = new File(getIdentity() + "_state.json");
-
-            if (!inputFile.exists()) {
-                printMsg("No previous state found. Creating new");
-
-                this.state = new MailboxState();
-            } else {
-                printMsg("Loading previous state");
-
-                try {
-                    this.state = jsonMapper.readValue(inputFile, MailboxState.class);
-                } catch (Exception ex) {
-
-                }
-            }
-
-            return true;
-        });
-    }
-
-    @Override
-    protected Task<Void> writeState() {
-        return Task.runAsync(() -> {
-            printMsg("Saving state");
-
-            File outputFile = new File(getIdentity() + "_state.json");
-            try {
-                jsonMapper.writeValue(outputFile, state());
-            } catch (Exception ex) {
-
-            }
-        });
-    }
-
-    @Override
-    protected Task<Void> clearState() {
-        return Task.supplyAsync(() -> {
-            printMsg("Clearning state");
-
-            this.state = new MailboxState();
-
-            return writeState();
-        });
     }
 
     @Override
@@ -85,23 +36,25 @@ public class MailboxActor extends AbstractActor<MailboxState> implements Mailbox
         newMessage.setDateTime(new Date());
         newMessage.setText(text);
 
-        state().getMessages().add(newMessage);
+        messages.add(newMessage);
 
-        return writeState();
+        return Task.done();
     }
 
     @Override
     public Task<List<Message>> getAllMessages() {
         printMsg("getAllMessages called");
 
-        return Task.fromValue(state().getMessages());
+        return Task.fromValue(messages);
     }
 
     @Override
     public Task<Void> deleteAllMessages() {
         printMsg("deleteAllMessages called");
 
-        return clearState();
+        messages.clear();
+
+        return Task.done();
     }
 
     private void printMsg(String pattern, Object[]... args) {
